88301040e87cb49d561837cc6a1398a323be6efb
J0j4t4n <94208326+J0j4t4n@users.noreply.github.com>
2025-08-09 22:03:03 -0500

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/proyecto_tec_adsi/', // Añadido para GitHub Pages
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
